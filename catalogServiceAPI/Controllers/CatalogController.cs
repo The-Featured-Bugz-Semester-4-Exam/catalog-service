24bb08d9d037670bce166426fac3b508e9f15ae7
Jonathan <eaajonhl@students.eaaa.dk>
2023-05-12 12:04:24 +0200

    First commit

---
+++ catalogServiceAPI/Controllers/CatalogController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace catalogServiceAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CatalogController : ControllerBase
+{
+    
+
+    private readonly ILogger<CatalogController> _logger;
+
+    public CatalogController(ILogger<CatalogController> logger)
+    {
+        _logger = logger;
+    }
+
+   
+}
